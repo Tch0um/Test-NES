423693f79b76a5fecc0b9a82fbbdba740de371c7
Goron Nathan <goron.nathan14@gmail.com>
2017-12-13 20:38:55 +0100

    Echange des valeurs et retour du résultat
    Dans le cas ou la valeur i est effectivement susceptible de remplir nos
    conditions si jamais échangé avec la valeur j , on effectue le
    changement et on retourne le résultat.
    Dans le cas contraire , on sort des boucles et on retourne un message.

--- preentrevue_GORON.py
+++ preentrevue_GORON.py
@@ -1,8 +1,21 @@
 from math import *
 
+
+
 def test():
     valeur = input("Selectionnez un entier de départ: ")
     valeur=valeur[::-1]                                  #On retourne le nombre renseigné afin de faciliter son traitement
     for i in range(0,len(valeur)-1):                     #Boucle au travers la liste des premier chiffre a comparer
         for j in range(i+1,len(valeur)):                 #Boucle au travers la liste des seconds chiffre a comparer
-            if valeur[i]>valeur[j]:                      #Comparaison des deux chiffres                                       
+            if valeur[i]>valeur[j]:                      #Comparaison des deux chiffres
+                tamplist=list(valeur)                    #Transformation de la de la valeur en liste afin d'effectuer les échanges de valeur
+                tamp=tamplist[j]                         #  ---
+                tamplist[j]=tamplist[i]                  # Échange des deux valeurs
+                tamplist[i]=tamp                         #  ---
+                tamplist=tamplist[::-1]                  #On retourne la liste dans le bon ordre avant de l'afficher
+                print(tamplist,"\n")
+                return tamplist
+    return("aucune valeur superieure avec les chiffres renseignés ") #Cas ou l'on a atteint la valeur maximale avec les chiffres renseignés
+                      
+        
+
