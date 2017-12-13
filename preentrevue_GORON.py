310bcb5d38cf364959717b922233f2cec3bba76b
Goron Nathan <goron.nathan14@gmail.com>
2017-12-13 20:36:15 +0100

    Comparaison
    On teste si la valeur en position i (dernier chiffre de notre nombre que
    l'on remonte ensuite dans la premiére boucle) est supérieure a la valeur
    en position j (valeur susceptible de nous fournir le résultat le plus
    proche du nombre initial)

--- preentrevue_GORON.py
+++ preentrevue_GORON.py
@@ -5,4 +5,4 @@ def test():
     valeur=valeur[::-1]                                  #On retourne le nombre renseigné afin de faciliter son traitement
     for i in range(0,len(valeur)-1):                     #Boucle au travers la liste des premier chiffre a comparer
         for j in range(i+1,len(valeur)):                 #Boucle au travers la liste des seconds chiffre a comparer
-                                       
+            if valeur[i]>valeur[j]:                      #Comparaison des deux chiffres                                       
