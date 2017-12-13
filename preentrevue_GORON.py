8c438ccc45848cbf5b4084772d9a9dda5041ddd8
Goron Nathan <goron.nathan14@gmail.com>
2017-12-13 20:32:02 +0100

    Création de la fonction
    -Prompt demandant un entier a l'utilisateur
    -Boucles traitant la comparaison des valeurs sans redondances

---
+++ preentrevue_GORON.py
@@ -0,0 +1,8 @@
+from math import *
+
+def test():
+    valeur = input("Selectionnez un entier de départ: ")
+    valeur=valeur[::-1]                                  #On retourne le nombre renseigné afin de faciliter son traitement
+    for i in range(0,len(valeur)-1):                     #Boucle au travers la liste des premier chiffre a comparer
+        for j in range(i+1,len(valeur)):                 #Boucle au travers la liste des seconds chiffre a comparer
+                                       
