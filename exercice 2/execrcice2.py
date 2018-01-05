af93eec9569d0f3e1580e6eeff0fbd9a54ebab15
Goron Nathan <goron.nathan14@gmail.com>
2018-01-05 19:27:01 +0100

    Variante avec réduction des prix
    Modification du programme pour implanter les deux réductions suivantes
    lors du calcul du prix du panier :    - 2 pommes achetées = 1 pomme
    supplémentaire offerte     - 3 oranges achetées = orange supplémentaire
    offerte

--- exercice 2/execrcice2.py
+++ exercice 2/execrcice2.py
@@ -2,14 +2,21 @@ from math import *
 
 def checkout(shoplist):
     total=0
+    pommes=0
+    oranges=0
     for i in range(0,len(shoplist)): #boucle de parcours de la liste prise en entrée
         if (shoplist[i].lower()) in ["pomme","0"]: #détection des pommes 
             total+=0.60
+            pommes+=1
         if (shoplist[i].lower()) in ["orange","1"]: #détection des oranges
             total+=1.25
-    print(total,"$") #affichage du total
+            oranges+=1
+            
+    print("total avant réduction:",total,"$") #affichage du total avant réduction
+    total-=((pommes//3)* 0.60 )+((oranges//4)* 1.25) #application de la réduction 
+    print("total aprés réduction:",total,"$") #affichage du total aprés réduction 
     return total
 
 # modifier la liste ci dessous pour changer le shopping cart
 # "pomme" ou "0" pour une pomme , "orange" ou "1" pour une orange 
-checkout(["Orange","1","PoMmE","0"])
+checkout(["Orange","1","PoMmE","0","orange"])
