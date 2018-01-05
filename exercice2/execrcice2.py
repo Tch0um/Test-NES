884e09075e6069848b084fc2881b1b2e935df2d8
Goron Nathan <goron.nathan14@gmail.com>
2018-01-05 18:44:57 +0100

    Fonction de calcul du panier
    Parcours d'une liste en entrée , on détecte les pommes par la présence
    de la chaine "pomme" (insensible a la casse) ou de la chaine "0".
    Fonctionnement similaire pour les oranges : "orange" ou "1".

--- exercice2/execrcice2.py
+++ exercice2/execrcice2.py
@@ -2,3 +2,11 @@ from math import *
 
 def checkout(shoplist):
     total=0
+    for i in range(0,len(shoplist)):
+        if (shoplist[i].lower()) in ["pomme","0"]:
+            total+=0.60
+        if (shoplist[i].lower()) in ["orange","1"]:
+            total+=1.25
+    print(total,"$")
+    return total
+
